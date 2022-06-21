e507911f13aaeb85466971db0c3b6d149d9da3bf
C0A21002 <c0a2100292@edu.teu.ac.jp>
2022-06-21 14:47:16 +0900

    03

--- calc.py
+++ calc.py
@@ -1,7 +1,26 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    tkm.showinfo("", f"{num}のボタンがクリックされました。")
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("電卓")
     root.geometry("300x500")
+    r, c = 0,0
+    for num in range(9, -1, -1):
+        btn = tk.Button(root,
+                        text=f"{num}",
+                        width=4,
+                        height=2,
+                        font=("Times New Roman", 30)
+                   ) 
+        btn.grid(row=r, column=c) 
+        c += 1
+        if (num-1)%3 ==0:
+            r += 1
+            c = 0 
+        btn.bind("<1>", button_click)
     root.mainloop()
