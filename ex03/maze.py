ac784c69e1e58cb9fe7415b80ec76875f25ab558
C0A21002 <c0a2100292@edu.teu.ac.jp>
2022-06-28 14:54:33 +0900

    sec2

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,4 +4,6 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     root.geometry("1500x900")
+    canvas = tk.Canvas(root, width = 1500, height= 900, bg = "black" )
+    canvas.pack()
     root.mainloop()
