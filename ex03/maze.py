c2785f12c94f0c75c66ecda9200375300e9f4f26
C0A21002 <c0a2100292@edu.teu.ac.jp>
2022-06-28 15:14:22 +0900

    sec3

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,4 +6,7 @@ if __name__ == "__main__":
     root.geometry("1500x900")
     canvas = tk.Canvas(root, width = 1500, height= 900, bg = "black" )
     canvas.pack()
+    tori = tk.PhotoImage(file = "fig/1.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, image=tori, tag="tori")
     root.mainloop()
