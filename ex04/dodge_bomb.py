2893a95f7a4484286f0790e184ec72832378d84a
C0A21002 <c0a2100292@edu.teu.ac.jp>
2022-07-05 16:30:30 +0900

    3限基本機能実装完了

---
+++ ex04/dodge_bomb.py
@@ -0,0 +1,80 @@
+import pygame as pg
+import sys
+import random
+
+def main():
+    clock = pg.time.Clock()
+
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen_sfc = pg.display.set_mode((1600, 900))
+    screen_rct = screen_sfc.get_rect()
+    being_sfc = pg.image.load("fig/pg_bg.jpg")
+    being_rct = being_sfc.get_rect()
+    screen_sfc.blit(being_sfc, being_rct)
+
+    kkimg_sfc = pg.image.load("fig/6.png")
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
+    kkimg_rct = kkimg_sfc.get_rect()
+    kkimg_rct.center = 900, 400
+
+    image = pg.Surface((20,20))
+    image.set_colorkey(0, 0)
+    pg.draw.circle(image, (255, 0, 0), (10,10), 10)
+    bomb_rct = image.get_rect()
+    a = random.randint(300, 1300)
+    b = random.randint(300, 600)
+    bomb_rct.center = a, b
+    vx, vy = 1, 1
+
+    while True:
+        screen_sfc.blit(being_sfc, being_rct)
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
+        bomb_rct.move_ip(vx, vy)
+        screen_sfc.blit(image, bomb_rct)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        if pg.key.get_pressed()[pg.K_UP]:
+             kkimg_rct.move_ip(0, -1)
+        if pg.key.get_pressed()[pg.K_DOWN]:
+            kkimg_rct.move_ip(0, 1)
+        if pg.key.get_pressed()[pg.K_LEFT]:
+            kkimg_rct.move_ip(-1, 0)
+        if pg.key.get_pressed()[pg.K_RIGHT]:
+             kkimg_rct.move_ip(1, 0)
+
+        if check_bound(kkimg_rct, screen_rct) != (1, 1):
+            if pg.key.get_pressed()[pg.K_UP]:
+                 kkimg_rct.move_ip(0, 1)
+            if pg.key.get_pressed()[pg.K_DOWN]:
+                kkimg_rct.move_ip(0, -1)
+            if pg.key.get_pressed()[pg.K_LEFT]:
+                kkimg_rct.move_ip(1, 0)
+            if pg.key.get_pressed()[pg.K_RIGHT]:
+                kkimg_rct.move_ip(-1, 0)
+        
+        yoko, tate = check_bound(bomb_rct,screen_rct)
+        vx *= yoko
+        vy *= tate
+
+        if kkimg_rct.colliderect(bomb_rct):
+            return
+
+            
+        pg.display.update()
+        clock.tick(1000)
+    
+def check_bound(rct, scr_rct):
+    yoko, tate = +1, +1
+
+    if rct.left < scr_rct.left or scr_rct.right  < rct.right  : yoko = -1  
+    if rct.top < scr_rct.top   or scr_rct.bottom < rct.bottom : tate = -1
+    return yoko, tate
+    
+
+if __name__ == '__main__':
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
+    
